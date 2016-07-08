9beb5bd1b544067270f447915b016439fee705a8
Samina Chhatwal <csaminak@gmail.com>
2016-07-08 13:52:30 -0400

    added html tags to li when a new todo is created

--- module1.js
+++ module1.js
@@ -3,19 +3,32 @@
 
     window.todo = ns = (window.todo || {})
 
-
+    //when page loads
     $('form').on('submit', function(event){
             event.preventDefault();
-            var listItem = $('input').val();
-            addListItem(listItem);
+            var todoItem = $('input').val();
+            addListItem(todoItem);
             $('.new-todo').val('');
     });
 
-
-    function addListItem (listItem) {
+    //add list item and the following html elements appear with it.
+    function addListItem (todoItem) {
         $('.items')
-            .append('<li>' + listItem + '</li>');
+            .append('<li class="listItem"></li>')
+            .find('li')
+                .append('<article></article>')
+                .find('article')
+                    .append('<button class="check"></button>')
+                    .append('<p>' + todoItem + '</p>') //todoItem will be replaced if edited and enter is selected
+                    .append('<button class="delete"></button>');
     }
+
+    //when listItem is clicked, the <p> turn into an input field.
+    $('.listItem').on('click', function edit() {
+
+        ('<input class="edit-todo" edit="text" value="">')
+
+    });
 
 
 
