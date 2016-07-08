fe4760d466f77689ba338eb8b9618980953ca319
Samina Chhatwal <csaminak@gmail.com>
2016-07-08 12:10:47 -0400

    made input value go into my .items class

--- module1.js
+++ module1.js
@@ -1,3 +1,29 @@
-(function(ns){
+(function(ns, $){
+    'strict';
 
-})(ns);
+    window.todo = ns = (window.todo || {})
+
+
+    $('form').on('submit', function(event){
+            event.preventDefault();
+            var listItem = $('input').val();
+            addListItem(listItem);
+    });
+
+
+    function addListItem (listItem) {
+        $('.items')
+            .append('<li>' + listItem + '</li>');
+    }
+
+
+
+
+
+
+
+
+
+
+
+})(window.todo, window.jquery);
