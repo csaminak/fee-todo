5fdf4a472b84c2961f9bfdd8203cc621f9cecc5e
Samina Chhatwal <csaminak@gmail.com>
2016-07-10 22:53:21 -0400

    moved around functions

--- module1.js
+++ module1.js
@@ -5,6 +5,7 @@
 
     ns.toDoList = [];
 
+
     $('form').on('submit', function(event){
             event.preventDefault();
             var toDoItem = $('input').val();
@@ -59,39 +60,6 @@
     }
 
 
-    $('.items').on('click', '.todoText', edit);
-    /**
-     * [When a user wants to edit a toDoItem they can click the item,
-     * then save it or they can escape the edit function by clicking the
-     * footer area or the header area]
-     * @param  {[event]}   [event is the specfic item that is clicked to edit]
-     * @return {[string]}  [an edited/new string or goes back to inital state]
-     */
-    function edit(event) {
-        $(event.target)
-            .replaceWith('<input class="editToDo" type="text" value=' + event.target.innerText + '></input>');
-
-        $('.editToDo').on('keypress', function(event) {
-            var editedItem = $('.editToDo').val();
-            if(event.keyCode === 13) {
-                $('.editToDo')
-                    .replaceWith('<p class="todoText">' + editedItem + '</p>')
-            }
-        });
-
-        if($('header').on('click', event.target, escape));
-        if($('footer').on('click', event.target, escape));
-        /**
-         * [allows user to escape the edit function]
-         * @return void
-         */
-        function escape() {
-            $('.editToDo')
-                .replaceWith('<p class="todoText">' + $('.editToDo').val() + '</p>');
-        }
-    }
-
-
     $('.items').on('click', '.check', markComplete);
     /**
      * [marks a toDoItem complete and then updates the object in
@@ -118,28 +86,6 @@
         incompleteCounter();
     }
 
-
-    $('footer').on('click', viewButtons);
-    /**
-     * [function that selects the code that will run depending on
-     * which button was clicked]
-     * @param  {[event]}  [button click]
-     * @return {[object]} [will return matching toDoList Items]
-     */
-    function viewButtons(event) {
-        console.log(event.target);
-        if (event.target.className === 'show-active') {
-            $('.complete').hide();
-            $('.items')
-                .addClass('incompleteItems');
-        } else if (event.target.className === 'show-completed') {
-            $('.incomplete').hide();
-        } else if (event.target.className === 'show-all-active') {
-            $('.toDoItem').show();
-        } else if (event.target.className === 'clear') {
-            $('.complete').remove();
-        }
-    }
 
 
 
--- module2.js
+++ module2.js
@@ -5,4 +5,60 @@
 
 
 
+    $('.items').on('click', '.todoText', edit);
+    /**
+     * [When a user wants to edit a toDoItem they can click the item,
+     * then save it or they can escape the edit function by clicking the
+     * footer area or the header area]
+     * @param  {[event]}   [event is the specfic item that is clicked to edit]
+     * @return {[string]}  [an edited/new string or goes back to inital state]
+     */
+    function edit(event) {
+        $(event.target)
+            .replaceWith('<input class="editToDo" type="text" value=' + event.target.innerText + '></input>');
+
+        $('.editToDo').on('keypress', function(event) {
+            var editedItem = $('.editToDo').val();
+            if(event.keyCode === 13) {
+                $('.editToDo')
+                    .replaceWith('<p class="todoText">' + editedItem + '</p>')
+            }
+        });
+
+        if($('header').on('click', event.target, escape));
+        if($('footer').on('click', event.target, escape));
+        /**
+         * [allows user to escape the edit function]
+         * @return void
+         */
+        function escape() {
+            $('.editToDo')
+                .replaceWith('<p class="todoText">' + $('.editToDo').val() + '</p>');
+        }
+    }
+
+
+
+    $('footer').on('click', viewButtons);
+    /**
+     * [function that selects the code that will run depending on
+     * which button was clicked]
+     * @param  {[event]}  [button click]
+     * @return {[object]} [will return matching toDoList Items]
+     */
+    function viewButtons(event) {
+        console.log(event.target);
+        if (event.target.className === 'show-active') {
+            $('.complete').hide();
+            $('.items')
+                .addClass('incompleteItems');
+        } else if (event.target.className === 'show-completed') {
+            $('.incomplete').hide();
+        } else if (event.target.className === 'show-all-active') {
+            $('.toDoItem').show();
+        } else if (event.target.className === 'clear') {
+            $('.complete').remove();
+        }
+    }
+
 })(window.todo);
